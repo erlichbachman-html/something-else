55320e3832d2081b57eeb5da9237f4634e6a7c6c
erlichbachman-html <226844650+erlichbachman-html@users.noreply.github.com>
2026-01-18 20:44:59 +0400

    Create jan-18.js

---
+++ jan-18.js
@@ -0,0 +1,32 @@
+let courses = [
+    { title: "JS", duration: 2, top: ["architecture", "C"] },
+    { title: "Python", duration: 3, top: ["data analysis", "automation", "architecture", "C"] },
+    { title: "React", duration: 4, top: ["components", "hooks"] },
+    { title: "Node.js", duration: 3, top: ["backend", "APIs", "C"] },
+    { title: "TypeScript", duration: 2, top: ["type safety", "OOP", "architecture", "C"] },
+    { title: "SQL", duration: 2, top: ["databases", "queries"] }
+]
+
+
+function getCourse() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(courses[Math.floor(Math.random() * courses.length)])
+        }, 4000)
+    })
+}
+
+
+async function getCourses() {
+    let course_one = await getCourse()
+    let course_two = await getCourse()
+    let result = {
+        title: course_one.title + "+"+ course_two.title,
+        duration: course_one.duration + course_two.duration,
+        top: [...new Set([...course_one.top, ...course_two.top])]
+    }
+    return result
+}
+
+getCourses().then((result)=>console.log(result))
+
